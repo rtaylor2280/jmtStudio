fbf7b706f309455de562961a6d5469b64fe9ad31
Ryan Taylor <rtaylor2280@gmail.com>
2026-05-06 13:35:45 -0600

    linux: use no-space productName to fix zygote launch crash

--- build/linux-after-install.sh
+++ build/linux-after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SANDBOX="/opt/JMT Studio/chrome-sandbox"
+SANDBOX="/opt/JMT-Studio/chrome-sandbox"
 if [ -f "$SANDBOX" ]; then
     chown root:root "$SANDBOX"
     chmod 4755 "$SANDBOX"
